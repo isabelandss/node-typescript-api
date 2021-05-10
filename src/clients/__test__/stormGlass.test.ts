81850ce2ce7981a14ce71cc65acb436820ef1662
Isabela Neri <isabelandss@gmail.com>
2021-05-10 11:22:28 -0300

    mockando dados

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/stormGlass'
+import axios from 'axios'
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
+import stormGlassNormalizedWeather3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json'
+
+jest.mock('axios')
+
+describe('StormGlass client', () => {
+  test('should return the normalizeed forecast from the StormGlass service', async () => {
+    const lat = -33.29283928
+    const lng = 152.81291829
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+
+    const stormGlass = new StormGlass(axios)
+    const response = await stormGlass.fetchPoints(lat, lng)
+
+    expect(response).toEqual(stormGlassNormalizedWeather3HoursFixture)
+  })
+})
